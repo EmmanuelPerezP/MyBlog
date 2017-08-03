1ceec5806548325aeaa5da6cba01e2e97aa1aea4
EmmanuelPerezP <emmanuelperezp@gmail.com>
2017-08-03 00:41:48 -0600

    agregar paginas nuevas

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.BlogPostListView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)$', views.BlogPost.as_view(), name='post-detail'),
+    url(r'^post/(?P<pk>\d+)$', views.BlogPost.as_view(), name='post-detail'),
+    url(r'^about/$', views.AboutMe.as_view(), name='about'),
+    url(r'^contact/$', views.Contact.as_view(), name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,18 @@ class BlogPostListView(generic.ListView):
     model = Post
     context_object_name = 'blog_posts'
     template_name = 'index.html'
+    paginate_by = 3
 
 
 class BlogPost(generic.DetailView):
     model = Post
     # context_object_name = 'post'
     template_name = 'post.html'
+
+
+class AboutMe(generic.TemplateView):
+    template_name = 'about.html'
+
+
+class Contact(generic.TemplateView):
+    template_name = 'contact.html'
