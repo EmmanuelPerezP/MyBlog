c521a646a6cf8d867981ddbb1488c7a92d376121
EmmanuelPerezP <emmanuelperezp@gmail.com>
2017-09-15 00:13:47 -0600

    cosas

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from django.views.generic import RedirectView
 urlpatterns = [
     url(r'^$', views.BlogPostListView.as_view(), name='index'),
     url(r'^post/(?P<pk>\d+)$', views.BlogPost.as_view(), name='post-detail'),
-    url(r'^acerca/$', views.AboutMe.as_view(), name='about'),
-    url(r'^contacto/$', views.Contact.as_view(), name='contact'),
+    url(r'^about/$', views.AboutMe.as_view(), name='about'),
+    url(r'^contact/$', views.Contact.as_view(), name='contact'),
     url(r'^new-post/$', views.Contact.as_view(), name='newPost'),
 ]
 
